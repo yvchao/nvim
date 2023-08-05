e365e21ebf14291bee371aea8347cdd7a5685ab0
Yuchao Qin <yvchao@outlook.com>
2023-08-05 18:38:00 +0100

    N. improve remote ui
    Signed-off-by: Yuchao <yvchao@outlook.com>

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -7,3 +7,15 @@ vim.cmd([[autocmd BufWritePost load.lua source <afile> | PackerCompile]])
 
 -- start insert when enter the terminal
 vim.cmd("autocmd TermOpen term://* startinsert")
+
+-- auto config font with remote ui
+vim.api.nvim_create_autocmd("UIEnter", {callback = function ()
+  if vim.v.event.chan > 1 then
+    if vim.g.loaded_clipboard_provider then
+      vim.o.guifont = "Iosevka Fixed SS14:h12"
+      vim.g.neovide_no_custom_clipboard = false
+      vim.g.loaded_clipboard_provider = nil
+      vim.api.nvim_cmd( { cmd = 'runtime', args = { 'autoload/provider/clipboard.vim' } }, {} )
+    end
+  end
+end})
