856903baf09bd5ebb0ad6fb25fe7a41cb8120eb9
Yuchao Qin <yvchao@outlook.com>
2023-10-30 15:15:28 +0000

    plugin: add block format utility for quarto files
    Signed-off-by: Yuchao <yvchao@outlook.com>

--- after/ftplugin/quarto.lua
+++ after/ftplugin/quarto.lua
@@ -3,6 +3,7 @@ local setup_done = vim.g["quarto_setup"] or false
 if not setup_done then
   local nmap = require("lib.keymap").nmap
   local utils = require("lib.repl")
+  local notify = require("lib.notify")
 
   -- append otter source to nvim-cmp
   local cmp = require("cmp")
@@ -14,6 +15,56 @@ if not setup_done then
   local luasnip = require("luasnip")
   luasnip.filetype_extend("quarto", { "markdown" })
   luasnip.filetype_extend("quarto", { "python" })
+
+  local otter_keeper = require("otter.keeper")
+  local fmt_util = require("formatter.util")
+  local fmt_config = require("formatter.config")
+  local fmt_format = require("formatter.format")
+  local fmt_log = require("formatter.log")
+
+  local function range_format(filetype, start_line, end_line, opts)
+    local modifiable = vim.bo.modifiable
+    if not modifiable then
+      notify.infoL("Buffer is not modifiable", "formatter")
+      return
+    end
+    start_line = start_line - 1
+    fmt_log.current_format_mods = opts and opts.smods or "verbose"
+    local formatters = fmt_config.formatters_for_filetype(filetype)
+
+    local configs_to_run = {}
+    -- No formatters defined for the given file type
+    if fmt_util.is_empty(formatters) then
+      notify.infoL(string.format("No formatter defined for %s files", filetype), "formatter")
+      return
+    end
+    for _, formatter_config in ipairs(formatters) do
+      local formatter
+      if type(formatter_config) == "table" then
+        formatter = formatter_config
+      else
+        formatter = formatter_config()
+      end
+      if formatter and formatter.exe then
+        table.insert(configs_to_run, { config = formatter, name = formatter.exe })
+      end
+    end
+    fmt_format.start_task(configs_to_run, start_line, end_line, opts)
+  end
+
+  vim.api.nvim_buf_create_user_command(0, "Format", function(args)
+    local filetype = args.args
+    local start_line = args.line1
+    local end_line = args.line2
+    range_format(filetype, start_line, end_line)
+  end, { range = "%", nargs = 1 })
+
+  nmap("<leader>bf", function()
+    local lang, row1, _, row2, _ = otter_keeper.get_current_language_context()
+    if lang ~= nil then
+      range_format(lang, row1 + 1, row2, { smods = "verbose" })
+    end
+  end)
 
   local function run_cell()
     local repl = utils.repls["Python"]
